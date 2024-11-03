3663119e8686f481b8127dca2c5802fe839af7f0
Tyson Paul <tysonpaul89@gmail.com>
2024-11-03 15:05:52 +0530

    Logger package updated with init function with Text handler

--- pkg/logger/log.go
+++ pkg/logger/log.go
@@ -2,6 +2,14 @@ package logger
 
 import "log/slog"
 
+// Global variable to store the slog.Logger object
+var logger *slog.Logger
+
+// This function is called only once when its imported and is used to initialize the global variable
+func init() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+}
+
 // Interface containing functions that can be used to log data.
 // To use logger you have to call NewLogger() function of this package.
 // Example:
@@ -15,31 +23,34 @@ type Logger interface {
 	Info() nil
 	Warn() nil
 	Error() nil
-} 
+}
 
 
 // Private sturct that implements the Logger class
 type loggerImpl struct {
+	log *slug.Logger
 }
 
 // Public function that returns the Logger object
 // When importing call this function to get the object of the Logger
 func NewLogger() Logger {
-	return loggerImpl{}
+	return loggerImpl{
+		log: logger
+	}
 }
 
 func (l *Logger) Debug(msg string, args ...any) {
-	slog.Debug(msg, args...)
+	l.log.Debug(msg, args...)
 }
 
 func (l *Logger) Info(msg string, args ...any) {
-	slog.Info(msg, args...)
+	l.log.Info(msg, args...)
 }
 
 func (l *Logger) Warn(msg string, args ...any) {
-	slog.Warn(msg, args...)
+	l.log.Warn(msg, args...)
 }
 
 func (l *Logger) Error(msg string, args ...any) {
-	slog.Error(msg, args...)
+	l.log.Error(msg, args...)
 }
