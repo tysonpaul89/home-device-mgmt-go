084ef926f2d28ca14c932be5341622245439c99c
Tyson Paul <tysonpaul89@gmail.com>
2024-11-05 22:42:06 +0530

    Lumberjack added to logger to implement the log rotation mechanism

--- cmd/hrdm/main.go
+++ cmd/hrdm/main.go
@@ -1,8 +1,11 @@
 package main
 
-import "hrdm/log"
+import "hrdm/pkg/log"
 
 // Main function initalizes, logger, config files, web servers, etc.
 func main() {
-  log.Info("Hello World!")
+	log.Info("This is an Info")
+	log.Warn("This is a Warn")
+	log.Debug("This is a Debug")
+	log.Error("This is a Error")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module hrdm
 
 go 1.21.4
+
+require gopkg.in/natefinch/lumberjack.v2 v2.2.1
